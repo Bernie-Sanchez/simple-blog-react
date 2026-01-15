cef92230451f68c1a4bdbe936e29ba0e463afd34
Bernie Sanchez <sanchez.bernie10@gmail.com>
2026-01-15 15:53:40 +0800

    Setup routing, pages structure, and project configuration

---
+++ src/pages/Login.tsx
@@ -0,0 +1,19 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form action="">
+                <input type="text" placeholder="Email" />
+                <br />
+
+                <input type="text" placeholder="Password" />
+                <br />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,19 @@
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            
+            <form action="">
+                <input type="text" placeholder="Email" />
+                <br />
+
+                <input type="password" placeholder="Password" />
+                <br />
+
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
